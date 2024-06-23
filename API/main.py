967e621ad717624e78c402a4170b8ddb35b973d1
GstavRocha <rocha.gustavo@escolar.ifrn.edu.br>
2024-06-23 18:20:42 -0300

    up

--- API/main.py
+++ API/main.py
@@ -1,7 +1,8 @@
+from typing import Union
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get('/')
-def checkApi():
-    return {'hello': 'world'}
+def getReady():
+    print({"hello":"world"})
